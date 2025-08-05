31283bdbbfa458b292739bf6cdc04c0792b5ade9
kazimzm1 <kazimzain32@gmail.com>
2025-08-05 09:57:34 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local placeId = game.PlaceId
 
 if placeId == 4520749081 or placeId == 6381829480 or placeId == 15759515082 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ChopLoris/ArcHub/main/run.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kazimzm1/fictional-spork/refs/heads/main/raid.lua"))()
 elseif placeId == 5931540094 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ChopLoris/ArcHub/main/raid.lua"))()
 end
